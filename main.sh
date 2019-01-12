0fd4bd1545a2c86f6497463a4194254d0177131b
Shogo Matsumoto <sougyo2001@gmail.com>
2019-01-12 15:56:32 +0900

    decorate ggplot graphs

--- main.sh
+++ main.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-rm -rf out && mkdir out
-
-ARCHIVE_DIR=rhel74
+ARCHIVE_DIR=work/rhel74
+OUTDIR_CSV=work/out
+OUTDIR_PLOT=work/plot
+METRICS_DIR=work/metrics
 ARCHIVE=$ARCHIVE_DIR/`grep ^Archive: $ARCHIVE_DIR/Latest | awk '{print $3}'`
+DURATION=60s
+STARTTIME=-3hour
 
-for path in metrics/* ; do
-  echo $path
+function make_csv() {
+  cat $2 | awk '{print $1}' | \
+    xargs pmrep -a $1 -f "%H:%M:%S" -o csv -t $DURATION -S $STARTTIME | \
+    sed '1s/^Time/time/'
+}
+
+echo processing csv...
+mkdir -p $OUTDIR_CSV
+for path in $METRICS_DIR/* ; do
+  echo " $path"
   f=`basename $path`
-  ./make_csv.sh $ARCHIVE $path > out/${f}.csv
+  make_csv $ARCHIVE $path > $OUTDIR_CSV/${f}.csv
+done
+
+echo ""
+echo processing plots...
+for path in $METRICS_DIR/* ; do
+  echo " $path"
+  f=`basename $path`
+  outdir=$OUTDIR_PLOT/$f
+  mkdir -p $outdir
+  ./plot.r $METRICS_DIR/$f $OUTDIR_CSV/${f}.csv $outdir 2>> $OUTDIR_PLOT/plot.log
 done
--- plot.r
+++ plot.r
@@ -20,12 +20,17 @@ csv         <- args[2]
 outdir      <- args[3]
 
 df <- read_csv(csv)
+df <- df %>%
+  filter(apply(!is.na(df %>% select(-time)), 1, any))
 
-top_n <- function(df, n = 5) {
-  cols <- select(df, -time) %>%
-    purrr::map_dbl(mean, na.rm=T)        %>%
-    .[head(order(., decreasing=T), n=n)] %>%
-    names()
+top_n <- function(df, n = 10) {
+  cols <- select(df, -time)       %>%
+    purrr::map_dbl(mean, na.rm=T) %>%
+    sort()                        %>%
+    rev()                         %>%
+    names()                       %>%
+    head(n=n)
+
   bind_cols(select(df, time, cols))
 }
 
@@ -39,20 +44,30 @@ for (l in read_lines(metric_list)) {
   if (metric == "") {
     break
   }
-  args   <- tail(record, n=-1)
+  args <- tail(record, n=-1)
 
   info = ""
   d <- select(df, contains(metric), time)
+
+  if (has_option(args, "fillna")) {
+    d[is.na(d)] <- 0
+  }
 
   if (has_option(args, "top")) {
     d <- top_n(d)
     info = " (top10)"
   }
 
-  p <- d %>%
-    gather("col", "val", -time) %>%
-    ggplot() + geom_line(mapping=aes(time, val, color=col)) +
-      labs(y=str_c(metric, info), color="")
+  p <- d %>% gather("col", "val", -time) %>% ggplot()
+  if (has_option(args, "stack")) {
+    p <- p + geom_area(mapping=aes(time, val, group=col, fill=col))
+  } else {
+    p <- p + geom_line(mapping=aes(time, val, color=col))
+  }
+  p <- p + ylim(0, NA) +
+         labs(y=str_c(metric, info), color="", fill="") +
+         theme(legend.position = "bottom", legend.direction = "vertical") +
+         scale_y_continuous(labels = scales::comma)
 
   path <- str_c(outdir, "/", metric, ".png")
   ggsave(file=path, plot=p)
